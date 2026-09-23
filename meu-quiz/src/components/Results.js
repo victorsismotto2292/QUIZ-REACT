6addd65b3c68eff0267d195c9c164a682483f744
= <167320276+victorsismotto2292@users.noreply.github.com>
2026-09-23 18:01:05 -0300

    Create Results.js

---
+++ meu-quiz/src/components/Results.js
@@ -0,0 +1,23 @@
+'use client';
+import React from 'react';
+
+const Results = ({ score, totalQuestions, onRestart }) => {
+  const percentage = Math.round((score / totalQuestions) * 100);
+
+  return (
+    <div className="quiz-card">
+      <h1>Resultado Final 🏆</h1>
+      <div className="result-score">
+        {score} / {totalQuestions}
+      </div>
+      <div className="result-percent">
+        Você acertou {percentage}% das questões!
+      </div>
+      <button className="btn-primary" onClick={onRestart}>
+        Refazer Quiz
+      </button>
+    </div>
+  );
+};
+
+export default Results;
