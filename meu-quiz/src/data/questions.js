2fd167eca186d7ceaaf32e5daecb5ba0cc5cab4f
= <167320276+victorsismotto2292@users.noreply.github.com>
2026-09-23 17:58:49 -0300

    Criando questões
    Questões para o quiz - Tema: Superheróis

---
+++ meu-quiz/src/data/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    id: 1,
+    question: "Qual super-herói é conhecido como o 'Homem de Aço'?",
+    options: ["Batman", "Superman", "Homem de Ferro", "Thor"],
+    correctAnswer: 1
+  },
+  {
+    id: 2,
+    question: "Qual a identidade secreta do Homem-Aranha?",
+    options: ["Bruce Wayne", "Clark Kent", "Peter Parker", "Tony Stark"],
+    correctAnswer: 2
+  },
+  {
+    id: 3,
+    question: "De qual planeta vem o Superman?",
+    options: ["Asgard", "Xandar", "Krypton", "Oa"],
+    correctAnswer: 2
+  },
+  {
+    id: 4,
+    question: "Qual destes itens é a arma principal do Thor?",
+    options: ["Escudo", "Mjolnir", "Anel de Poder", "Batarang"],
+    correctAnswer: 1
+  },
+  {
+    id: 5,
+    question: "Quem é o rei de Wakanda?",
+    options: ["T'Challa", "Shuri", "M'Baku", "Killmonger"],
+    correctAnswer: 0
+  },
+  {
+    id: 6,
+    question: "Qual super-herói usa um anel que cria construções de luz verde?",
+    options: ["Flash", "Lanterna Verde", "Aquaman", "Shazam"],
+    correctAnswer: 1
+  },
+  {
+    id: 7,
+    question: "Quem criou o traje do Homem de Ferro?",
+    options: ["Steve Rogers", "Bruce Banner", "Tony Stark", "Reed Richards"],
+    correctAnswer: 2
+  },
+  {
+    id: 8,
+    question: "Qual é a principal característica do Flash?",
+    options: ["Força Sobre-humana", "Voo", "Super-velocidade", "Telepatia"],
+    correctAnswer: 2
+  }
+];
+
+export default questions;
